f635a425df379f206f5c8b4a91e2e17c9b6b9b47
Santiago Acevedo <sacevedor@unbosque.edu.co>
2021-10-17 18:24:12 -0500

    initializing

--- src/co/edu/unbosque/controller/Controller.java
+++ src/co/edu/unbosque/controller/Controller.java
@@ -1,5 +1,19 @@
 package co.edu.unbosque.controller;
 
+import co.edu.unbosque.model.Fibonacci;
+import co.edu.unbosque.model.MatrixMultiplication;
+import co.edu.unbosque.view.View;
+
 public class Controller {
+	
+	public Controller() {
+		Fibonacci fibo = new Fibonacci();
+		MatrixMultiplication matrix = new MatrixMultiplication();
+		View view = new View();
+	}
+	
+	public void run() {
+		
+	}
 
 }
--- src/co/edu/unbosque/view/View.java
+++ src/co/edu/unbosque/view/View.java
@@ -1,5 +1,11 @@
 package co.edu.unbosque.view;
 
 public class View {
+	
+	public void ShowInfo(String msg) {
+		System.out.println(msg);
+		
+	}
+	
 
 }
